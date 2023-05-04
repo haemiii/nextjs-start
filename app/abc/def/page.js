976250ca9858fdaac9f497b231ebb27c5643e55d
haemiii <73984822+haemiii@users.noreply.github.com>
2023-05-04 17:05:40 +0900

    routing

---
+++ app/abc/def/page.js
@@ -0,0 +1,7 @@
+export default function def() {
+  return (
+    <div>
+      <h1 className="title">def페이지에용~~</h1>
+    </div>
+  );
+}
--- app/page.js
+++ app/page.js
@@ -1,12 +1,12 @@
+import Link from "next/link";
+
 export default function Home() {
   let name = "kang";
   let link = "http://google.com";
   return (
     <div>
       <h4 className="title">애플후레시</h4>
-      <h4 style={{ color: "red", fontSize: "30px" }}>애플후레시</h4>
       <p className="title-sub">by dev {name}</p>
-      <a href={link}>링크</a>
     </div>
   );
 }
