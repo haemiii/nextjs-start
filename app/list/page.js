e29a1d70c62906204c003dd2ea9fe05ecb3384d9
haemiii <73984822+haemiii@users.noreply.github.com>
2023-05-04 17:42:52 +0900

    img tag

--- app/list/page.js
+++ app/list/page.js
@@ -3,16 +3,12 @@
 // list폴더 생성 후 page.js 파일 생성
 // /list 접속시 page.js의 html 전송됨!
 
+import Image from "next/image";
+// import tomato from "/public/tomato.png";
+
 export default function List() {
   let 상품 = ["Tomatoes", "Pasta", "Coconut"];
   let array = [2, 3, 4];
-  let a = array.map((a, i) => {
-    console.log(a); // 2, 3 ,4
-    console.log(i); // 0, 1, 2
-
-    return 10;
-  });
-  console.log(a); // [10, 10, 10]
 
   return (
     <div>
@@ -20,7 +16,9 @@ export default function List() {
       {상품.map((a, i) => {
         return (
           //return이 없으면 그냥 코드를 읽고 지나감!
-          <div className="food">
+          <div className="food" key={i}>
+            <img src={`food${i}.png`} className="food-img"></img>
+            {/* <Image src={tomato} className="food-img" /> */}
             <h4>{상품[i]} $40</h4>
           </div>
         );
